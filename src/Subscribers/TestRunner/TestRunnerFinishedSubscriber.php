0005d6f3020a650f66d81c135a47fbc638f43962
Tomas Votruba <tomas.vot@gmail.com>
2024-01-24 10:51:35 +0100

    misc

--- src/Subscribers/TestRunner/TestRunnerFinishedSubscriber.php
+++ src/Subscribers/TestRunner/TestRunnerFinishedSubscriber.php
@@ -10,6 +10,7 @@ use PHPUnit\Event\TestRunner\Finished;
 use PHPUnit\Event\TestRunner\FinishedSubscriber;
 use PHPUnit\Metadata\DataProvider;
 use PHPUnit\TestRunner\TestResult\Facade;
+use PHPUnit\TestRunner\TestResult\TestResult;
 use TomasVotruba\PHPUnitJsonResultPrinter\Printer\SimplePrinter;
 
 final class TestRunnerFinishedSubscriber implements FinishedSubscriber
@@ -39,28 +40,7 @@ final class TestRunnerFinishedSubscriber implements FinishedSubscriber
             ],
         ];
 
-        $resultJsonData['failed'] = [];
-
-        // print failed tests
-        foreach ($testResult->testFailedEvents() as $testFailedEvent) {
-            /** @var Failed $testFailedEvent */
-            $testMethod = $testFailedEvent->test();
-
-            /** @var TestMethod $testMethod */
-            $failedEventData = [
-                'test_class' => $testMethod->className(),
-                'test_method' => $testMethod->methodName(),
-                'message' => $testFailedEvent->throwable()->message(),
-                'exception_class' => $testFailedEvent->throwable()->className(),
-                'line' => $this->resolveLineNumber($testFailedEvent->throwable()->stackTrace()),
-            ];
-
-            if ($testMethod->testData()->hasDataFromDataProvider()) {
-                $failedEventData['data_provider'] = $this->createDataProviderData($testMethod);
-            }
-
-            $resultJsonData['failed'][] = $failedEventData;
-        }
+        $resultJsonData['failed'] = $this->createFailedEventDatas($testResult);
 
         $resultJson = json_encode($resultJsonData, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
         $this->simplePrinter->writeln($resultJson);
@@ -91,5 +71,35 @@ final class TestRunnerFinishedSubscriber implements FinishedSubscriber
     {
         preg_match('#:(?<line>\d+)$#', $stackTrace, $matches);
         return (int) $matches['line'];
+    }
+
+    /**
+     * @return Failed[]
+     */
+    private function createFailedEventDatas(TestResult $testResult): array
+    {
+        $failedEventDatas = [];
+
+        foreach ($testResult->testFailedEvents() as $testFailedEvent) {
+            /** @var Failed $testFailedEvent */
+            $testMethod = $testFailedEvent->test();
+
+            /** @var TestMethod $testMethod */
+            $failedEventData = [
+                'test_class' => $testMethod->className(),
+                'test_method' => $testMethod->methodName(),
+                'message' => $testFailedEvent->throwable()->message(),
+                'exception_class' => $testFailedEvent->throwable()->className(),
+                'line' => $this->resolveLineNumber($testFailedEvent->throwable()->stackTrace()),
+            ];
+
+            if ($testMethod->testData()->hasDataFromDataProvider()) {
+                $failedEventData['data_provider'] = $this->createDataProviderData($testMethod);
+            }
+
+            $failedEventDatas[] = $failedEventData;
+        }
+
+        return $failedEventDatas;
     }
 }
